abcd2dd7b26bb26adc4bf474a30ec76e70d8b375
MagedMaher550 <magedmaher373@gmail.com>
2026-07-04 20:43:28 +0300

    chore: configure initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-950 text-4xl font-bold text-white">
+      Bundle Builder
+    </div>
+  );
+}
+
+export default App;
